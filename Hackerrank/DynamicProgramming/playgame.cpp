8118640ae3c3696892e8310aa9fc8d0c5f2e582b
Vibhor Kanojia <versatilevibhor@gmail.com>
2014-10-12 06:52:08 +0530

    PlayGame : Done ; tip -> 50k recursive calls=>you are fucked

--- Hackerrank/DynamicProgramming/playgame.cpp
+++ Hackerrank/DynamicProgramming/playgame.cpp
@@ -5,37 +5,28 @@ using namespace std;
 
 #define long long long
 
-void fillBricks(vector<int> & bricks, int N,long & sum){
+void fillBricks(vector<int> & bricks, int N){
 
 	for (int i = 0 ; i < N ; i++){
 		int num;
 		cin >> num;
 		bricks.push_back(num);
-		sum = sum + num;
 	}
 }
 
-long returnMax(long a1,long a2,long a3){
-	long temp_max = a1 >= a2 && a1 >= a3 ? a1 : a2 >= a3 ? a2 : a3;
+long returnMin(long a1,long a2,long a3){
+	long temp_max = a1 <= a2 && a1 <= a3 ? a1 : a2 <= a3 ? a2 : a3;
 	return temp_max;
 }
 
-long applyDP(vector<int> & bricks, int N, int index, long sum,vector<long> & array){
-	if (index>N-1){
-		return 0;
+long applyDP(vector<int> & bricks,int N, vector<long> & array){
+	long sum = 0;
+	for (int i = N-1 ; i >= 0 ; i--){
+		sum = sum + bricks[i];
+		array[i] = sum - returnMin(array[i+1], array[i+2], array[i+3]);
 	}
 	
-	long score=0;
-	if (array[index] != -1){
-		return array[index];
-	}
-	long val1 = applyDP(bricks, N, index+1, sum - bricks[index], array );
-	long val2 = applyDP(bricks, N, index+2, sum - bricks[index] - bricks[index+1], array);
-	long val3 = applyDP(bricks, N, index+3, sum - bricks[index] - bricks[index+1] - bricks[index+2], array);
-	score = returnMax(sum - val1,sum - val2,sum - val3);
-	array[index] = score;
-	return score;
-	
+	return array[0];
 }
 
 
@@ -46,11 +37,9 @@ int main(){
 		int N;
 		cin >> N;
 		vector<int> bricks;
-		long sum=0;
-		vector<long> array = *new vector<long>(N,-1);
-		fillBricks(bricks,N,sum);
-		cout<<"hello"<<endl;
-		long score = applyDP(bricks,N,0, sum,array);
+		vector<long> array = *new vector<long>(N+3,0);
+		fillBricks(bricks,N);
+		long score = applyDP(bricks,N, array);
 		cout<<score<<endl;
 		
 	}
