ff67114afd2383e9d7957d2645ff55bf386a2b81
Vibhor Kanojia <versatilevibhor@gmail.com>
2014-11-03 11:22:10 +0530

    Finallygit add --all * Never ever use bool in a vector

--- Codechef/primegenerator.cpp
+++ Codechef/primegenerator.cpp
@@ -2,75 +2,60 @@
 
 using namespace std;
 
-#define MYMAX  10005
-struct data{
-	long long val;
-	bool flag;
-};
+#define MYMAX  33000
 
 
-void sieve(vector <struct data> & myvector){
+void sieve(vector <int> & yourvector, vector<int> & myvector){
 	for (int i =2 ; i < MYMAX ; i++){
-		if (!myvector[i].flag) continue;
+		if (!yourvector[i]) continue;
 		else{
 			for (int j = 2*i ; j< MYMAX ; j=j+i){
-				myvector[j].flag = 0;
+				yourvector[j] = 0;
 			}
 		}
+	}
+	for (int i=2 ; i < MYMAX ; i++){
+		if (yourvector[i])
+			myvector.push_back(i);
 	}
 }
 
 
 
 int main(){
-	vector<struct data> myvector;
+	vector<int> yourvector;
+	vector<int> myvector;
 	for (long long i = 0 ; i < MYMAX ; i++){
-		struct data d;
-		d.val = i;
 		if (i == 0 || i == 1)
-			d.flag = 0;
-		else d.flag = 1;
-		myvector.push_back(d);
+			yourvector.push_back(0);
+		else 
+			yourvector.push_back(1);
 	}
-	sieve(myvector);
-
+	sieve(yourvector, myvector);
 	int testcases;
 	cin >>testcases;
 	while (testcases--){
 		long long M,N;
 		cin >>M;
 		cin >>N;
-	
-		if (N <= MYMAX){
-			for (long long i = M ; i <= N ; i++){
-				if (!myvector[i].flag) continue;
-				cout<<myvector[i].val<<endl;
-			}
-			return 0;
-		}
-
 		long long j;
 		bool myflag;
 		long long mysqrt;
 		for (long long i = M ; i <=N ; i++){
-			if (i%2 == 0 || i%3 == 0) continue;
-			j = myvector[0].val;
+			if (i == 1) continue;
+			
+			j =0;
 			myflag = 0;
 			mysqrt = sqrt(i)+1;
-			//cout<<"calculated "<<i<<" "<<sqrt(i)<<endl;
-			while (j < mysqrt){
-				if (!myvector[j].flag) {
-					j++;
-					continue;
-				}
-				if (i%myvector[j].val == 0){
+			while (myvector[j] < mysqrt){
+				if (i%myvector[j] == 0){
 					myflag = 1;
 					break;
 				}
 				j++;
 			}
 			if (myflag == 0){
-				cout<<i<<endl;
+				printf("%lld\n",i);
 			}
 		}
 	}
